a675c36d29489f55ca9d41a8b79bb89d37bbac70
evilicebergs <voiced.fair-0a@icloud.com>
2024-11-18 18:19:57 +0200

    add actor instead of class, and adapt an app for it

--- foo/BankTransferApp.swift
+++ foo/BankTransferApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct fooApp: App {
+struct BankTransferApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
--- foo/ContentView.swift
+++ foo/ContentView.swift
@@ -7,10 +7,56 @@
 
 import SwiftUI
 
+enum BankError: Error {
+    case insufficientFunds(Double)
+}
+
+actor BankAccount {
+    
+    let accountNumber: Int
+    var balance: Double
+    
+    init(accountNumber: Int, balance: Double) {
+        self.accountNumber = accountNumber
+        self.balance = balance
+    }
+    
+    func deposit(_ amount: Double) {
+        balance += amount
+    }
+    
+    func transfer(amount: Double, to other: BankAccount) async throws {
+        if amount > balance {
+            throw BankError.insufficientFunds(amount)
+        }
+        
+        balance -= amount
+        await other.deposit(amount)
+        
+        print(other.accountNumber)
+        print("Current Account: \(balance), Other Account: \(await other.balance)")
+    }
+}
+
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button {
+            
+            let bankAccount = BankAccount(accountNumber: 123, balance: 500)
+            let otherAccount = BankAccount(accountNumber: 456, balance: 100)
+            
+            DispatchQueue.concurrentPerform(iterations: 100) { _ in
+                Task {
+                    try? await bankAccount.transfer(amount: 300, to: otherAccount)
+                }
+            }
+            
+        } label: {
+            Text("Transfer")
+        }
+
     }
 }
 
